437b5ee2c625a0bd41ae5522f28000d3e9bbbd53
Đinh Tuấn Nghĩa <dinhtuannghia35@gmail.com>
2021-04-19 22:39:52 +0700

    change-demo

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,19 +1,35 @@
-let langue = 'vi'
-let num = 1
-let baseURL =`https://api.themoviedb.org/3/discover/movie?api_key=1b79d7a7bdb69f136a8a39dcc2514e85&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`+`&language=${langue}`
+let langue = 'en'
+let num = 5
+let baseURL =`https://api.themoviedb.org/3/discover/movie?api_key=1b79d7a7bdb69f136a8a39dcc2514e85&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=${num}&with_watch_monetization_types=flatrate`+`&language=${langue}`
 let IMGPATH = "https://image.tmdb.org/t/p/w1280"
+let center = document.querySelector('.movie .container .row')
+let modalLoad = document.querySelector('.modal')
 
 
 fetch(baseURL).then(data=>data.json())
 .then((info)=>{
     callBack(info)
 })
-function callBack(num){
-     console.log(num);
-    // await console.log(num.results);
-    num.results.forEach((nums) => {
-        let img = document.createElement("img")
-        img.src = IMGPATH + nums.backdrop_path
-        document.body.appendChild(img)
-    });
+async function callBack(num){
+     
+    await window.addEventListener('load',contentLoad)
+    await num.results.forEach((nums) => {
+        let col = document.createElement('div')
+        col.classList.add('col-lg-3', 'col-md-6' ,'col-sm-12')
+        col.innerHTML = `
+            <div class="movie-box">
+            <a href="" class="movie-box__img"><img src="${IMGPATH+nums.backdrop_path}"></a>
+            <div class="movie-box__info">
+                    <p class="box__info-title"><a href="" class="info-title__link">${nums.original_title}</a></p>
+                    <span class="box__info-rating">${nums.vote_average}</span>
+                </div>
+            </div>
+        `
+        center.appendChild(col)
+
+    })
+    
+}
+function contentLoad(){
+    modalLoad.classList.add('active')
 }
