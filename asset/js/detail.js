74fa411ea4d57e3a58bd676f6d2e8bcd8e33b0c3
Đinh Tuấn Nghĩa <dinhtuannghia35@gmail.com>
2021-04-21 22:56:13 +0700

    change

--- asset/js/detail.js
+++ asset/js/detail.js
@@ -3,21 +3,118 @@ let id = localStorage.getItem("id")
 let IMGPATH = "https://image.tmdb.org/t/p/original"
 let detail = document.querySelector('.detail')
 let langue = 'en'
+let status =false
 
 
+let type = [`tv`,`movie`]
+let modalLoad = document.querySelector('.modalLoading')
 let baseURL = `https://api.themoviedb.org/3/movie/${id}?api_key=1b79d7a7bdb69f136a8a39dcc2514e85` + `&language=${langue}`
-fetch(baseURL).then(data => data.json())
-    .then(info => showDetail(info))
-async function showDetail(de) {
+let recom = `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=1b79d7a7bdb69f136a8a39dcc2514e85` + `&language=${langue}`
+let trailer = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=1b79d7a7bdb69f136a8a39dcc2514e85`+`&language=${langue}`
+let director = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=1b79d7a7bdb69f136a8a39dcc2514e85`+`&language=${langue}`
+let TV = `https://api.themoviedb.org/3/tv/${id}?api_key=1b79d7a7bdb69f136a8a39dcc2514e85`+`&language=${langue}`
+let recomTV = `https://api.themoviedb.org/3/tv/${id}/recommendations?api_key=1b79d7a7bdb69f136a8a39dcc2514e85&language=en-US&page=1`
+let trailerTV = `https://api.themoviedb.org/3/tv/${id}/videos?api_key=1b79d7a7bdb69f136a8a39dcc2514e85&language=en-US`
+let directorTV = `https://api.themoviedb.org/3/tv/${id}/credits?api_key=1b79d7a7bdb69f136a8a39dcc2514e85&language=en-US`
+function API(link1,link2, func1,func2) {
+    this.link1 = link1
+    this.link1 = link2
+    this.func1 = function(){
+
+    }
+    this.func2 = function(){
+
+    }
+    this.call = async () =>{
+            try{
+                let fetching = await fetch(link1)
+                let fetching2 = await fetch(link2)
+                let checkType = localStorage.getItem("type")
+                // console.log(checkType=undefined);
+                // console.log(checkType!=undefined);
+                // console.log(fetching.ok == true);
+                // console.log(fetching2.ok == true);
+                // console.log(checkType === undefined && fetching.ok == true);
+                // console.log(checkType!==undefined && fetching2.ok == true);
+                if (checkType!==undefined && fetching2.ok == true) {
+                    
+                    
+                    let data = await fetching2.json()
+                    
+                    func2(data)
+                    status = true
+                    checkData()
+                }
+                else {
+                    
+                    let data = await fetching.json()
+                    func1(data)
+                    status = true
+                    checkData()
+                } 
+            }
+            catch{(err)=>console.log(err)}
+        }
+        
+        function checkData(){
+            if (status===false) {
+                modalLoad.classList.add('active')
+            } else {
+                modalLoad.classList.remove('active')
+            }
+        }
+        checkData()
+    }
+    
+let dirrect = new API(director,directorTV,showDirect,showDirect)
+let trai = new API(trailer,trailerTV,showTrailer,showTrailer)
+let detai = new API(baseURL,TV,showDetail,showDetailTV)
+let reco = new API(recom,recomTV,showRecoment,showRecoment)
+detai.call()
+reco.call()
+trai.call()
+dirrect.call()
+function showDirect(dr){
+    
+    let create = dr.crew
+    console.log(create);
+    let reulst =  create.filter((creates)=>{
+        
+        return creates.known_for_department === "Directing"
+    })
+    console.log(reulst);
+    filterDir(reulst);
+}
+function filterDir(direct){
+    let director = document.querySelector('.detail-box__director')
+    
+    
+    direct.forEach(directs => {
+        let xyz = document.createElement('span')
+        xyz.innerHTML =`<a href=''>${directs.name}</a>`
+        
+        director.appendChild(xyz)
+        // xyz.innerText=des.name
+        // gener.appendChild(xyz)
+        // parent.appendChild(xyz)
+    });
+    director.appendChild(html) 
+}
+function showRecoment(re){
+    console.log(re);
+}
+function showTrailer(tl){
+    console.log(tl);
+}
+function showDetail(de) {
     let gernder = de.genres
-    console.log(de);
     let box = document.createElement("div")
     box.classList.add('container')
     detail.style.background = `linear-gradient(to top, rgba(255, 255, 255, 0.842) 0%, rgba(255, 255, 255, 0.137) 100%),url('${IMGPATH+de.backdrop_path}')`
     detail.style.backgroundRepeat = 'no-repeat'
     detail.style.backgroundSize = 'cover'
     detail.style.backgroundPosition = 'top'
-
+    
     detail.appendChild(box)
     
      box.innerHTML = `
@@ -40,19 +137,15 @@ async function showDetail(de) {
 
                 <div class="detail-box__group">
                     <p class="detail-box__date">${de.release_date}</p>
-                    <ul>
-                        ${
-                            showgender(gernder)
-                        }
+                    <ul class='detail-box__gender'>
+                        
                     </ul>
 
                     <p class="detail-box__time">${de.runtime} minutes</p>
                 </div>
                 <div class="detail-box__director">
-                    <span>Creator:</span>
-                    <span>nghia</span>
-                    <span>san</span>
-                    <span>hoang</span>
+                    <span>Director:</span>
+                    
                 </div>
                     <p class="detail-box__score">Rating: ${de.vote_average}</p>
                     <p class="detail-box__descript">Overview: "${de.overview}"</p>
@@ -61,24 +154,81 @@ async function showDetail(de) {
         </div>
     </div>
     `
-    let gender = document.querySelector('.detail-box__group ul')
-    console.log(gender);
-    showgender(gernder,gender)
-    function showgender(de,parent){
-        
-        
-        // let gener = document.querySelector('.detail-box__group')
-        // console.log(gener);
+    let gender = document.querySelector('.detail-box__gender')
+    
+    
+    function showgender(de){
         de.forEach(des => {
             let xyz = document.createElement('li')
-            xyz.innerHTML =`${des.name}`
-            console.log(xyz);
+            xyz.innerHTML =`<a href=''>${des.name}</a>`
+            gender.appendChild(xyz)
+            
+        });
+    }
+    showgender(gernder)
+}
+
+function showDetailTV(de) {
+    
+    let gernder = de.genres
+    let box = document.createElement("div")
+    box.classList.add('container')
+    detail.style.background = `linear-gradient(to top, rgba(255, 255, 255, 0.842) 0%, rgba(255, 255, 255, 0.137) 100%),url('${IMGPATH+de.backdrop_path}')`
+    detail.style.backgroundRepeat = 'no-repeat'
+    detail.style.backgroundSize = 'cover'
+    detail.style.backgroundPosition = 'top'
+    
+    detail.appendChild(box)
+    
+     box.innerHTML = `
+    <div class="detail-box">
+    <div class="row">
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="detail-box__img">
+                <img src="${IMGPATH+de.poster_path}" alt=""
+                    class="">
+            </div>
+        </div>
+        <div class="col-lg-8 col-md-12 col-sm-12">
+            <div class="detail-box__detail">
+                <p class="detail-box__name">
+                    ${de.name}
+                </p>
+                <p class="detail-box__name">
+                    ${de.original_name}
+                </p>
+                <p class="detail-box__date">Season:${de.number_of_seasons}</p>
+                <p class="detail-box__date">Episode:${de.number_of_episodes}</p>
+                <div class="detail-box__group">
+                    <p class="detail-box__date">${de.first_air_date}</p>
+                    <ul class='detail-box__gender'>
+                        
+                    </ul>
+
+                    <p class="detail-box__time">${de.episode_run_time} minutes/ 1 Episode </p>
+                </div>
+                <div class="detail-box__director">
+                    <span>Director:</span>
+                    
+                </div>
+                    <p class="detail-box__score">Rating: ${de.vote_average}</p>
+                    <p class="detail-box__descript">Overview: "${de.overview}"</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    let gender = document.querySelector('.detail-box__gender')
+    function showgender(de){
+        de.forEach(des => {
+            let xyz = document.createElement('li')
+            xyz.innerHTML =`<a href=''>${des.name}</a>`
+            
+            gender.appendChild(xyz)
             // xyz.innerText=des.name
             // gener.appendChild(xyz)
             // parent.appendChild(xyz)
         });
-        
     }
-    
+    showgender(gernder)
 }
-
--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,11 @@
 let langue = 'en'
-let num 
+let num = 1
+console.log(num);
+
+
+console.log(num);
 let baseURL =`https://api.themoviedb.org/3/discover/movie?api_key=1b79d7a7bdb69f136a8a39dcc2514e85&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=${num}&with_watch_monetization_types=flatrate`+`&language=${langue}`
+// let baseURL =`https://api.themoviedb.org/3/discover/tv?api_key=1b79d7a7bdb69f136a8a39dcc2514e85&language=en-US&sort_by=popularity.desc&page=${num}&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate`+`&language=${langue}`
 let IMGPATH = "https://image.tmdb.org/t/p/original"
 let center = document.querySelector('.movie .container .row')
 let modalLoad = document.querySelector('.modalLoading')
@@ -11,7 +16,7 @@ let count = 0
 async function fetchAPI(){
     let abc = await fetch(baseURL)
     let xyz = await abc.json()
-    console.log(xyz);
+    
     callBack(xyz)
     // setTimeout()
     
@@ -44,7 +49,7 @@ function callBack(num){
             <div class="movie-box">
                 <a href = './html/side-page.html' class="movie-box__img"><img src="${IMGPATH+nums.poster_path}"></a>
                 <div class="movie-box__info">
-                    <p class="box__info-title"><a href = './html/side-page.html' class="info-title__link">${nums.original_title}</a></p>
+                    <p class="box__info-title"><a href = './html/side-page.html' class="info-title__link">${nums.title}</a></p>
                     <span class="box__info-rating">${nums.vote_average}</span>
                 </div>
             </div>
@@ -74,6 +79,7 @@ async function getID(id){
 
 
 function clickFn(arr){
+    console.log(arr);
     // console.log(arr[0].id);
     let box = document.querySelectorAll('.movie-box')
     let link = document.querySelectorAll('.movie-box .movie-box__img')
@@ -85,6 +91,7 @@ function clickFn(arr){
             // link.href  = './html/side-page.html'
             // if (typeof(Storage) !== "undefined") {
                 localStorage.setItem("id",arr[i].id)
+                localStorage.setItem("type",arr[i].first_air_date)
             // } else {
                 
             // }
@@ -100,7 +107,26 @@ function clickFn(arr){
     
     
 }
-for (let index = 0; index> 0; index++) {
+function createPage(){
+    let page =  document.createElement('ul')
+    document.body.appendChild(page)
+    page.classList.add('moviepage')
+    let pages = [1,2,3,4,5]
+    for (let i = 0; i < pages.length; i++) {
+        let nums = pages[i];
+        page.innerHTML += `<li data-value=${nums} >${nums}</li>`
+        
+    }
     
     
+    // console.log(pagi);
+    
+    let pagi = document.querySelectorAll('.moviepage li')
+    console.log(pagi);
+    // console.log(page);
 }
+
+
+
+
+createPage()
